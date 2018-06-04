c01c48147a2c4cb78a4d4cf164f1d371714a231b
zgf123 <277845885@qq.com>
2018-06-04 10:47:16 +0000

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Auth::routes();
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 

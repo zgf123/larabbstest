4fbdc49060235ec27754534223b0cc2b05bc1d21
zgf123 <277845885@qq.com>
2018-06-04 04:24:41 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,12 +9,17 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
     public function edit(User $user){
+        $this->authorize('update', $user);
     	return view('users.edit', compact('user'));
     }
 

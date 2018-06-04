40d06f78044c9da7eb6547b52c84ec55091b8313
zgf123 <277845885@qq.com>
2018-06-04 10:00:04 +0000

    Author can delete topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,6 +9,11 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
+        return $user->isAuthorOf($topic);
+    }
+
+    public function destroy(User $user, Topic $topic)
+    {
+        return $user->isAuthorOf($topic);
     }
 }

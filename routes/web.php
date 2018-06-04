5e7815505095e74fb0c47543f79964cb6721a1a0
zgf123 <277845885@qq.com>
2018-06-04 05:47:59 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

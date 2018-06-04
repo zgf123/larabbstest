8d5718b28265c985b023170c17e8944bea924e67
zgf123 <277845885@qq.com>
2018-06-04 04:03:15 +0000

    User can upload avatar

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,13 +1,14 @@
 <?php
+
 namespace App\Handlers;
+
 use Image;
 
 class ImageUploadHandler
 {
-	// 只允许以下后缀名的图片文件上传
     protected $allowed_ext = ["png", "jpg", "gif", 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
         // 文件夹切割能让查找效率更高。
@@ -20,7 +21,7 @@ class ImageUploadHandler
         // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
-        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID 
+        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
         // 值如：1_1493521050_7BVc9v9ujP.png
         $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
 
@@ -29,15 +30,15 @@ class ImageUploadHandler
             return false;
         }
 
+        // 将图片移动到我们的目标存储路径中
+        $file->move($upload_path, $filename);
+
         // 如果限制了图片宽度，就进行裁剪
         if ($max_width && $extension != 'gif') {
 
             // 此类中封装的函数，用于裁剪图片
             $this->reduceSize($upload_path . '/' . $filename, $max_width);
         }
-
-        // 将图片移动到我们的目标存储路径中
-        $file->move($upload_path, $filename);
 
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
